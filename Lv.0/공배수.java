2a3846f83af10020bfbba967ceb717728321054d
gyongyeon <126961013+Cloudyee@users.noreply.github.com>
2023-05-16 19:24:31 +0900

    Update 공배수.java

--- Lv.0/공배수.java
+++ Lv.0/공배수.java
@@ -18,7 +18,7 @@
 class Solution {
     public int solution(int number, int n, int m) {
         int answer = 0;
-        if(number%n==0&&number%n==0){
+        if(number%n==0&&number%m==0){
             answer=1;
         }else{
             answer=0;
