129b617f1df9bf02ee2b2f7b39a0d2221241d86e
gyongyeon <126961013+Cloudyee@users.noreply.github.com>
2023-05-23 19:22:50 +0900

    Create 문자열 섞기.java

---
+++ Lv.0/문자열 섞기.java
@@ -0,0 +1,30 @@
+// 문제 설명
+// 길이가 같은 두 문자열 str1과 str2가 주어집니다.
+
+// 두 문자열의 각 문자가 앞에서부터 서로 번갈아가면서 한 번씩 등장하는 문자열을 만들어 return 하는 solution 함수를 완성해 주세요.
+
+// 제한사항
+// 1 ≤ str1의 길이 = str2의 길이 ≤ 10
+// str1과 str2는 알파벳 소문자로 이루어진 문자열입니다.
+// 입출력 예
+// str1	str2	result
+// "aaaaa"	"bbbbb"	"ababababab"
+
+class Solution {
+    public String solution(String str1, String str2) {
+        String answer = "";
+        for(int i=0; i<str1.length();i++){
+            answer+=str1.substring(i,i+1);
+            answer+=str2.substring(i,i+1);
+        }
+        System.out.println(answer);
+        return answer;
+    }
+}
+
+//----------------------------------
+//앞서 사용한 substring(int a, int b)활용
+
+//아쉬운점
+//솔루션을 스스로 생각해 내지 못했다.
+//생각보다 풀이가 단순할 수 있으니, 솔루션을 찾아보기 보단 먼저 해결법을 고민하는습관을 들이기
