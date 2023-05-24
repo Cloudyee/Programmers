061c999c1dda64b570758c344b5002587f83b56b
gyongyeon <126961013+Cloudyee@users.noreply.github.com>
2023-05-25 00:22:04 +0900

    Create 두 수의 합.java

---
+++ Lv.0/두 수의 합.java
@@ -0,0 +1,20 @@
+// 제한사항
+// -50,000 ≤ num1 ≤ 50,000
+// -50,000 ≤ num2 ≤ 50,000
+// 입출력 예
+// num1	num2	result
+// 2	3	5
+// 100	2	102
+// 입출력 예 설명
+// 입출력 예 #1
+
+// num1이 2이고 num2가 3이므로 2 + 3 = 5를 return합니다.
+// 입출력 예 #2
+
+// num1이 100이고 num2가 2이므로 100 + 2 = 102를 return합니다.
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = num1+num2;
+        return answer;
+    }
+}
